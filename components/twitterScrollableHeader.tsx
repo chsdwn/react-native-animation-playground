7f9bd32f54b83f3decf2b4d5ee96d609ec09a7e2
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-11 17:52:25 +0300

    Refactored with react native reanimated.

--- components/twitterScrollableHeader.tsx
+++ components/twitterScrollableHeader.tsx
@@ -1,12 +1,8 @@
-import React, { useState } from "react";
-import {
-  Animated,
-  Image,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import Animated from "react-native-reanimated";
+
+const { Extrapolate, Value } = Animated;
 
 const HEADER_MAX_HEIGHT = 120;
 const HEADER_MIN_HEIGHT = 70;
@@ -14,18 +10,18 @@ const PROFILE_IMAGE_MAX_HEIGHT = 80;
 const PROFILE_IMAGE_MIN_HEIGHT = 40;
 
 export const TwitterScrollableHeader = () => {
-  const [scrollY, setScrollY] = useState(new Animated.Value(0));
+  let scrollY = new Value(0);
 
   const headerHeight = scrollY.interpolate({
     inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
     outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
-    extrapolate: "clamp",
+    extrapolate: Extrapolate.CLAMP,
   });
 
   const profileImageHeight = scrollY.interpolate({
     inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
     outputRange: [PROFILE_IMAGE_MAX_HEIGHT, PROFILE_IMAGE_MIN_HEIGHT],
-    extrapolate: "clamp",
+    extrapolate: Extrapolate.CLAMP,
   });
 
   const profileImageMarginTop = scrollY.interpolate({
@@ -34,13 +30,13 @@ export const TwitterScrollableHeader = () => {
       HEADER_MAX_HEIGHT - PROFILE_IMAGE_MAX_HEIGHT / 2,
       HEADER_MAX_HEIGHT + 5,
     ],
-    extrapolate: "clamp",
+    extrapolate: Extrapolate.CLAMP,
   });
 
   const headerZIndex = scrollY.interpolate({
     inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
     outputRange: [0, 1],
-    extrapolate: "clamp",
+    extrapolate: Extrapolate.CLAMP,
   });
 
   const headerTitleBottom = scrollY.interpolate({
@@ -51,64 +47,73 @@ export const TwitterScrollableHeader = () => {
       HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT + 5 + PROFILE_IMAGE_MIN_HEIGHT + 26,
     ],
     outputRange: [-20, -20, -20, 0],
-    extrapolate: "clamp",
+    extrapolate: Extrapolate.CLAMP,
   });
 
-  return (
-    <View style={{ flex: 1 }}>
-      <Animated.View
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          backgroundColor: "lightskyblue",
-          height: headerHeight,
-          alignItems: "center",
-          zIndex: headerZIndex,
-        }}
-      >
-        <Animated.View
-          style={{ position: "absolute", bottom: headerTitleBottom }}
-        >
-          <Text style={{ fontSize: 14, fontWeight: "bold", color: "white" }}>
-            Ali Veli
-          </Text>
-        </Animated.View>
-      </Animated.View>
-
-      <ScrollView
-        style={{ flex: 1 }}
-        scrollEventThrottle={16}
-        scrollEnabled
-        onScroll={Animated.event([
-          { nativeEvent: { contentOffset: { y: scrollY } } },
-        ])}
-      >
+  if (
+    headerHeight &&
+    profileImageHeight &&
+    profileImageMarginTop &&
+    headerZIndex &&
+    headerTitleBottom
+  ) {
+    return (
+      <View style={{ flex: 1 }}>
         <Animated.View
           style={{
-            height: profileImageHeight,
-            width: profileImageHeight,
-            borderRadius: PROFILE_IMAGE_MAX_HEIGHT / 2,
-            borderColor: "white",
-            borderWidth: 3,
-            overflow: "hidden",
-            marginTop: profileImageMarginTop,
-            marginLeft: 10,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            backgroundColor: "lightskyblue",
+            height: headerHeight,
+            alignItems: "center",
+            zIndex: headerZIndex,
           }}
         >
-          <Image
-            source={require("../assets/profilePicture.png")}
-            style={{ flex: 1, width: "100%", height: "100%", zIndex: 0 }}
-          />
+          <Animated.View
+            style={{ position: "absolute", bottom: headerTitleBottom }}
+          >
+            <Text style={{ fontSize: 14, fontWeight: "bold", color: "white" }}>
+              Ali Veli
+            </Text>
+          </Animated.View>
         </Animated.View>
-        <View>
-          <Text style={{ fontWeight: "bold", fontSize: 26, paddingLeft: 10 }}>
-            Ali Veli
-          </Text>
-        </View>
-        <View style={{ height: 1000 }}></View>
-      </ScrollView>
-    </View>
-  );
+
+        <Animated.ScrollView
+          style={{ flex: 1 }}
+          scrollEventThrottle={16}
+          scrollEnabled
+          onScroll={Animated.event([
+            { nativeEvent: { contentOffset: { y: scrollY } } },
+          ])}
+        >
+          <Animated.View
+            style={{
+              height: profileImageHeight,
+              width: profileImageHeight,
+              borderRadius: PROFILE_IMAGE_MAX_HEIGHT / 2,
+              borderColor: "white",
+              borderWidth: 3,
+              overflow: "hidden",
+              marginTop: profileImageMarginTop,
+              marginLeft: 10,
+            }}
+          >
+            <Image
+              source={require("../assets/profilePicture.png")}
+              style={{ flex: 1, width: "100%", height: "100%", zIndex: 0 }}
+            />
+          </Animated.View>
+          <View>
+            <Text style={{ fontWeight: "bold", fontSize: 26, paddingLeft: 10 }}>
+              Ali Veli
+            </Text>
+          </View>
+          <View style={{ height: 1000 }}></View>
+        </Animated.ScrollView>
+      </View>
+    );
+  }
+  return null;
 };
