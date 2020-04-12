d95433b9cf6421d448d92b390d8f850858d3656c
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-12 18:49:03 +0300

    Background and sign in button animations completed.

--- components/animatedLogin.tsx
+++ components/animatedLogin.tsx
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, SyntheticEvent } from "react";
 import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 import { AppLoading } from "expo";
 import { Asset } from "expo-asset";
-import Animated from "react-native-reanimated";
+import Animated, { Easing } from "react-native-reanimated";
+import {
+  State,
+  TapGestureHandler,
+  TapGestureHandlerStateChangeEvent,
+} from "react-native-gesture-handler";
 
 const cacheImages = (images: [any]) => {
   return images.map((image) => {
@@ -15,9 +20,88 @@ const cacheImages = (images: [any]) => {
 };
 
 const { width, height } = Dimensions.get("window");
+const {
+  block,
+  clockRunning,
+  cond,
+  debug,
+  eq,
+  event,
+  interpolate,
+  set,
+  startClock,
+  stopClock,
+  timing,
+  useCode,
+  Clock,
+  Extrapolate,
+  Value,
+} = Animated;
+
+function runTiming(clock: Animated.Clock, value: any, dest: any) {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    frameTime: new Value(0),
+  };
+
+  const config = {
+    duration: 1000,
+    toValue: new Value(0),
+    easing: Easing.inOut(Easing.ease),
+  };
+
+  return block([
+    cond(
+      clockRunning(clock),
+      [
+        // if the clock is already running we update the toValue, in case a new dest has been passed in
+        set(config.toValue, dest),
+      ],
+      [
+        // if the clock isn't running we reset all the animation params and start the clock
+        set(state.finished, 0),
+        set(state.time, 0),
+        set(state.position, value),
+        set(state.frameTime, 0),
+        set(config.toValue, dest),
+        startClock(clock),
+      ]
+    ),
+    // we run the step here that is going to update position
+    timing(clock, state, config),
+    // if the animation is over we stop the clock
+    cond(state.finished, debug("stop clock", stopClock(clock))),
+    // we made the block return the updated position
+    state.position,
+  ]);
+}
 
 export const AnimatedLogin = () => {
   const [isReady, setIsReady] = useState(false);
+
+  const state = new Value(State.UNDETERMINED);
+  const buttonOpacity = new Value(1);
+
+  const buttonY = interpolate(buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [100, 0],
+    extrapolate: Extrapolate.CLAMP,
+  });
+  const bgY = interpolate(buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [-height / 3, 0],
+  });
+
+  useCode(
+    () =>
+      cond(
+        eq(state, State.END),
+        set(buttonOpacity, runTiming(new Clock(), 1, 0))
+      ),
+    [state, buttonOpacity]
+  );
 
   const _loadAssetsAsync = async () => {
     const imageAssets = cacheImages([require("../assets/bg.jpg")]);
@@ -37,26 +121,45 @@ export const AnimatedLogin = () => {
 
   return (
     <View style={{ flex: 1, justifyContent: "flex-end" }}>
-      <View style={{ width: "100%", height: "100%", position: "absolute" }}>
+      <Animated.View
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          transform: [{ translateY: bgY }],
+        }}
+      >
         <Image
           source={require("../assets/bg.jpg")}
           style={{ flex: 1, height: "100%", width: "100%" }}
         />
-      </View>
+      </Animated.View>
       <View style={{ height: height / 3 }}>
-        <View style={styles.button}>
-          <Text style={{ fontSize: 20, fontWeight: "bold" }}>SIGN IN</Text>
-        </View>
-        <View
+        <TapGestureHandler
+          onHandlerStateChange={Animated.event([{ nativeEvent: { state } }])}
+        >
+          <Animated.View
+            style={{
+              ...styles.button,
+              opacity: buttonOpacity,
+              transform: [{ translateY: buttonY }],
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>SIGN IN</Text>
+          </Animated.View>
+        </TapGestureHandler>
+        <Animated.View
           style={{
             ...styles.button,
             backgroundColor: "#2e71dc",
+            opacity: buttonOpacity,
+            transform: [{ translateY: buttonY }],
           }}
         >
           <Text style={{ fontSize: 20, fontWeight: "bold", color: "white" }}>
             SIGN IN WITH FACEBOOK
           </Text>
-        </View>
+        </Animated.View>
       </View>
     </View>
   );
